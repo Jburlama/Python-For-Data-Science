23f1a2783d67b8171d90ee81a37a98f6820a77b7
jhonas <jhonas.burlamaqui@gmail.com>
2025-10-10 14:48:17 +0100

    4-Dod ex00

---
+++ 4-Dod/ex00/statistics.py
@@ -0,0 +1,36 @@
+def ft_statistics(*args: any, **kwargs: any) -> None:
+    stats = {
+        "mean": 0,
+        "median": None,
+        "quartile": None,
+        "std": None,
+        "var": None,
+    }
+
+    try:
+        for i in args:
+            stats["mean"] += i
+        stats["mean"] = stats["mean"] / len(args)
+        stats["median"] = args[int(len(args) / 2) - 1]
+        sort = sorted(args)
+        stats["quartile"] = (sort[int(len(sort) / 4)] * 1.0,
+                             sort[int(len(sort) / 1.3)] * 1.0)
+
+        dist_from_mean = [pow(stats["mean"] - i, 2) for i in args]
+        var = 0
+        for i in dist_from_mean:
+            var += i
+        var /= len(args)
+        stats["var"] = var
+
+        std = var ** 0.5
+        stats["std"] = std
+
+        for i in kwargs:
+            if kwargs[i] in stats:
+                print(f"{kwargs[i]}: {stats[kwargs[i]]}")
+
+    except ZeroDivisionError:
+        for i in kwargs:
+            if kwargs[i] in stats:
+                print("ERROR")
